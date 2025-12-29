672a2ba383e6b3a41efe0ba50301a23e53a48670
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-12-29 17:57:11 -0400

    feat: cambios

---
+++ src/app/usuario/page/listar-usuario/listar-usuario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-usuario',
+  imports: [],
+  templateUrl: './listar-usuario.html',
+  styleUrl: './listar-usuario.css',
+})
+export class ListarUsuario {
+
+}
